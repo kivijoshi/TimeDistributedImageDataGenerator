21e01d1935bb355e7761849cad64699787a90f8e
Kaustubh <joshi.kaustubh@zf.com>
2020-05-02 15:19:05 +0200

    Release v0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'TimeDistributedImageDataGenerator',
   packages = ['TimeDistributedImageDataGenerator'],
-  version = '0.1',
+  version = '0.2',
   license='MIT',
   description = 'Extension of Keras ImageDataGenerator class for TimeDistributed layer support.',
   author = 'Kaustubh Joshi',
